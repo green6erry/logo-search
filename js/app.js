71e989900fd1b46964d92fc8d68a2537bca5ef47
green6erry <green6erry@gmail.com>
2016-04-29 14:03:57 -0400

    Half working
    A lot

--- js/app.js
+++ js/app.js
@@ -1,5 +1,54 @@
   $(function() {
-    
+
+console.log('page load');
+
 $('#category').selectable();
 
-  });
+
+
+function  criteria(category, minSalary){
+  //if(salary >= $('#logo-list').find('img[salary="'++'"]'))
+
+  var checkCategory = $('#logo-list').find('img[value="'+category+'"]');
+  checkCategory.removeClass('gray');
+  
+  var logoSalary = parseInt($('#logo-list img').attr('alt'));
+  
+  var checkSalary = logoSalary <= minSalary;
+  console.log('salary', checkSalary)
+
+
+  
+
+
+
+  if(checkSalary && checkCategory) {
+    
+  }
+
+  
+  
+  
+
+
+ //console.log($('#logo-list img').get());
+
+}
+
+$('#content').submit(function(){
+  console.log('hi');
+  event.preventDefault();
+  var genreSelected = $('#content').find('li[class="ui-selectee ui-selected"]').val();
+  var salarySelected = parseInt($('#salary').val());
+  console.log(genreSelected, salarySelected);
+  criteria(genreSelected, salarySelected);
+});
+
+
+
+
+function showResults() {}
+
+ });
+
+ 
